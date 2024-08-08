70d5ea5bfa828acd0ce13283a85b4e68d22259a4
subham2020btecs00002 <subhamcoolkumaryadav@gmail.com>
2024-08-09 02:10:00 +0530

    commit

--- config/db.js
+++ config/db.js
@@ -3,11 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://subhamkumar22082001:gQVaXIxJEIUU2net@cluster0.ycrz8tu.mongodb.net/trade?retryWrites=true&w=majority&appName=Cluster0",
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
+      "mongodb+srv://subhamkumar22082001:gQVaXIxJEIUU2net@cluster0.ycrz8tu.mongodb.net/trade?retryWrites=true&w=majority&appName=Cluster0"
     );
     console.log("MongoDB connected");
   } catch (err) {
